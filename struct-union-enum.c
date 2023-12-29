3a6d59c472d218ee613535494a9429d9b4168602
unian0 <2532815199@qq.com>
2023-12-29 22:07:16 +0800

    struct-union-enum

--- struct-union-enum.c
+++ struct-union-enum.c
@@ -7,10 +7,20 @@ struct node
   int c;
 } node1;
 
+enum
+{
+  yi = 1,
+  er,
+  san,
+  si,
+  wu,
+} month;
 int main()
 {
   node1.val = 1;
   node1.c = 2;
+  printf("%d\n", er);
+  printf("%d\n", month);
   printf("node.c=%d\n", node1.c);
   return 0;
 }
